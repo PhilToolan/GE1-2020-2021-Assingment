01418e5f7c33d8d3afe6ecb711bc7cdd5ff53acf
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-18 19:36:15 +0000

    Floor and ceiling paddle working in Pong

--- Assingment/Assets/Scripts/PongPlayer.cs
+++ Assingment/Assets/Scripts/PongPlayer.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PongPlayer : MonoBehaviour
 {
 
     public Camera camera2D;
+    public LayerMask layerMaskFloor;
+    public LayerMask layerMaskWall;
+    public GameObject paddleFloor;
+    public GameObject paddleWall;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +30,30 @@ public class Player : MonoBehaviour
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, transform.right, 20, layerMaskFloor);
+        Debug.DrawRay(transform.position, transform.right * 20, Color.green);
+
+        RaycastHit2D hitWall = Physics2D.Raycast(transform.position, transform.right, 20, layerMaskWall);
+        Debug.DrawRay(transform.position, transform.right * 20, Color.green);
+
+        //If something was hit, the raycast.collider will not be null
+        if (hitFloor.collider != null)
+        {
+            if (hitFloor.point.y < 0)
+            {
+                paddleFloor.transform.position = new Vector3(hitFloor.point.x, -4.88f);
+            }
+            else //Snap to ceiling
+            {
+                paddleFloor.transform.position = new Vector3(hitFloor.point.x, 4.88f);
+            }
+        }
+
+        //If something was hit, the raycast.collider will not be null
+        if (hitWall.collider != null)
+        {
+            Debug.Log(hitWall.collider.name);
+        }
     }
 }
