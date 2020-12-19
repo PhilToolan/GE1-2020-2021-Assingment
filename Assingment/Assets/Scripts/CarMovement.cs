41a91492c292b0f9c5de0f9cc5ad16347d796b63
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-19 16:51:22 +0000

    Added PostProcessing to retrowave

---
+++ Assingment/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class CarMovement : MonoBehaviour
+{
+
+    public float speed = 50.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Drive(float units)
+    {
+        Vector3 forward = transform.forward;
+        //forward.y = 0;
+        forward.Normalize();
+        transform.position += forward * units;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float contDrive = Input.GetAxis("Vertical");
+        Drive(speed * Time.deltaTime);
+    }
+}
