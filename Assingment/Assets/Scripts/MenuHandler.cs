13fdfc84305c499fe1591df079c51e650f71e6ad
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-19 12:37:15 +0000

    Added Menu and Renamed Pong scene

---
+++ Assingment/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    [SerializeField] private string pongEffect;
+    [SerializeField] private string retrowave;
+    [SerializeField] private string theClub;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadPong()
+    {
+        SceneManager.LoadScene(pongEffect);
+    }
+
+    public void LoadRetro()
+    {
+        SceneManager.LoadScene(retrowave);
+    }
+
+    public void LoadClub()
+    {
+        SceneManager.LoadScene(theClub);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
